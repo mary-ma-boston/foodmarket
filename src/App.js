ef0fd3dd83f1a4a87c7296175a454d59938f8fe6
mayue <mayue@mayues-MacBook-Pro.local>
2023-07-23 23:03:15 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+
+import FoodList from './components/FoodList';
 import './App.css';
+import classes from './App.module.css';
+import {v4 as uuidv4} from 'uuid';
 
 function App() {
+  const [foodLists, setFoodLists] = useState([]);
+  const [food, setFood] = useState({
+    id: '',
+    name:'',
+    price:'',
+    origin:'',
+  });
+  const [isEdit, setIsEdit] = useState(false);
+
+  const inputHandler = (e) => {
+    
+    setFood({...food, [e.target.name]:e.target.value});
+  };
+
+  const submitHandler = (e) => {
+    
+    e.preventDefault();
+    
+    if(!isEdit) {
+      let newId = uuidv4();
+      let newFood = {...food, id: newId};
+      setFood(newFood);
+      let newFoodLists = [ ...foodLists, newFood];
+   
+      setFoodLists(newFoodLists);
+    } else {
+      // let filteredFoodLists = foodLists.filter(item=>item.id !== food.id);
+      // let newFoodLists = [food, ...filteredFoodLists];
+      const currentIndex = foodLists.findIndex(item => item.id === food.id);
+      const frontArray = foodLists.slice(0,currentIndex);
+      const endArray = foodLists.slice(currentIndex+1);
+      const newFrontFoodLists = [...frontArray, food];
+      let newFoodLists = [...newFrontFoodLists, ...endArray];
+
+      setFoodLists(newFoodLists);
+      setIsEdit(false);
+    }
+    
+    setFood({
+      id: '',
+      name:'',
+      price:'',
+      origin:'',
+    });  
+  };
+
+  const editFoodHandler = (editId) => {
+    
+      setIsEdit(true);
+      const findEditItem = (foodLists.filter(item => item.id === editId))[0];
+     
+      setFood(findEditItem);
+  };
+
+  const deleteFoodHandler = (deleteId) => {
+    
+    const filteredLists = foodLists.filter(item=>item.id !== deleteId);
+    setFoodLists(filteredLists);
+
+    setFood({
+      id: '',
+      name:'',
+      price:'',
+      origin:'',
+    }); 
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.container}>
+      <h1>Food Market</h1>
+      <div className={classes.listContainer}>
+        {foodLists.map(item=>{
+          return <FoodList 
+                    key={item.id} 
+                    foodItem={item} 
+                    editFoodHandler={editFoodHandler}  
+                    deleteFoodHandler={ deleteFoodHandler}
+                  />
+        })}
+      </div>
+      <div className={classes.formContainer}>
+        <form onSubmit={submitHandler} >
+          <div className={classes.inputStyle}>
+            <label>Name:</label>
+            <input type='text' value={food.name} name='name' onChange={inputHandler}/>
+          </div>
+          <div className={classes.inputStyle}>
+            <label>Price:</label>
+            <input type='text' value={food.price} name='price' onChange={inputHandler}/>
+          </div>
+          <div className={classes.inputStyle}>
+            <label>Origin:</label>
+            <input type='text' value={food.origin} name='origin' onChange={inputHandler}/>
+          </div>
+          <button type='submit'>{!isEdit? 'ADD':'Update'}</button>
+        </form>
+      </div>  
     </div>
   );
 }
---
+++ src/components/FoodList.jsx
@@ -0,0 +1,28 @@
+import classes from './FoodList.module.css';
+
+const FoodList = ({foodItem, editFoodHandler,  deleteFoodHandler}) => {
+    const onEditFoodHandler = (foodItemEditId)=> {
+       
+        editFoodHandler(foodItemEditId);
+    };
+
+    const onDeleteHandler = (event,foodDeleteId)=>{
+        event.stopPropagation();
+
+        deleteFoodHandler(foodDeleteId);
+        
+    }
+
+    return (
+        <div className={classes.listCon} onClick={()=>onEditFoodHandler(foodItem.id)}>
+            <div className={classes.foodListCon}>
+                <button onClick={(event)=>onDeleteHandler(event,foodItem.id)}>delete</button>
+                <div className={classes.listStyle}>Name:{foodItem.name}</div>
+                <div className={classes.listStyle}>Price:{foodItem.price}</div>
+                <div className={classes.listStyle}>Origin:{foodItem.origin}</div>
+            </div>
+        </div>   
+    )
+}
+
+export default FoodList;
